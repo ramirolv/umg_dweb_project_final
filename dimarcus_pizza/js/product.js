c07e7c360487e02e719bde018ca0a88bf7bb249d
ramirolv <rlopezv11@miumg.edu.gt>
2022-09-05 13:00:02 -0600

    Agregado de funcionalidad al formulario
    En el panel de menú se agregó funcionalidad al formulario de manera que se agregan los nuevos platillos donde corresponden.

--- dimarcus_pizza/js/product.js
+++ dimarcus_pizza/js/product.js
@@ -1,19 +1,50 @@
 $(document).on('ready', iniciar);
 
+//campos para rellenar
+var linkImg;
+var selectTipo;
+var inputSabor;
+var inputPrecio;
+
 function iniciar(){
-    $varBotonAgregar = $('#botonNuevo');
-    $varBotonAgregar.on('click', abrirFormulario);
-    
-    $varBotonSalirFromulario = $('#cerrarFormulario');
-    $varBotonSalirFromulario.on('click', cerrarFormulario);
-
-    $varformulario = $('#formularioAgregar');
+    //boton de agregar
+    $btnAgregar = $('#btnAgregar');
+    $btnAgregar.on('click', agregar);
 }
 
-function abrirFormulario(){
-    $varformulario.show();
+function agregar(){
+    //campos para rellenar
+    linkImg = document.getElementById('inputLinkImg').value;
+    selectTipo = document.getElementById('selectTipo').value;
+    inputSabor = document.getElementById('inputSabor').value;
+    inputPrecio = document.getElementById('inputPrecio').value;
+
+    if(validarCamposFormulario()){
+        if (selectTipo === 'Pizza') {
+            $contenedorProductos = $('#contenedorProductos1');
+        }else if(selectTipo === 'Otros'){
+            $contenedorProductos = $('#contenedorProductos2');
+        }
+        else if(selectTipo === 'Bebidas'){
+            $contenedorProductos = $('#contenedorProductos3');
+        }
+        $contenedorProductos.append(insertarProducto(linkImg, inputSabor, inputPrecio));
+        alert('Agregado con éxito');
+    }
 }
 
-function cerrarFormulario(){
-    $varformulario.hide();
+function validarCamposFormulario(){
+    alert('Entra a la función validar');
+    alert(linkImg+inputSabor+inputPrecio);
+    if (linkImg === '' || inputSabor === '' || inputPrecio === '') {
+        alert('No se han llenado todos los campos');
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function insertarProducto(img, sabor, precio){
+    var cadena = '<div class="col-lg-3 col-md-8 "> <div class="product-item d-flex flex-column bg-white rounded overflow-hidden h-100"> <div class="position-relative mt-auto">            <img class="img-fluid" src="'+img+'" alt="">        </div>        <div class="text-center p-2">                                <h4 class="mb-3">'+sabor+'</h4>            <span>Venta por Unidad</span>            <hr><div class="d-inline-block border border-primary rounded-pill px-2 mb-2">Q '+precio+'</div>        </div>                     </div></div>';
+    return cadena;
 }
