b826d21414e0a21ccdefc5461d2fcfe56b4c9490
ramirolv <rlopezv11@miumg.edu.gt>
2022-11-27 22:27:26 -0600

    config mysql client

--- umg_dweb_project_final/settings.py
+++ umg_dweb_project_final/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'umg_dweb_project_final.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'hED3ghCSwox9xst3dGAk',
+        'HOST': 'containers-us-west-20.railway.app',
+        'PORT': '7010'
     }
 }
 
