c3ca05d3ddd62d0249b0fb39e2542cebf8efb8a5
ramirolv <rlopezv11@miumg.edu.gt>
2022-11-27 20:31:35 -0600

    Update urls to Login init

--- umg_dweb_project_final/urls.py
+++ umg_dweb_project_final/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', include('apps.home.urls', 'index')),
+    path('', include('apps.home.urls', 'index')),
    
 
 ]
