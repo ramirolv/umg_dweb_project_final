8da24f5e3ab2dc6ba596a3ed21e1c761101dfbeb
ramirolv <rlopezv11@miumg.edu.gt>
2022-11-27 23:15:00 -0600

    Key csrf

--- umg_dweb_project_final/settings.py
+++ umg_dweb_project_final/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+
 import os
 from pathlib import Path
 from django.urls import reverse_lazy
@@ -21,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
 #SECRET_KEY = 'django-insecure-^pbx(-_hxzk$frtn_@i936%^c8gac!=2=0xuyk8^$fk47ac(=7'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -129,7 +130,7 @@ LOGIN_REDIRECT_URL = reverse_lazy('home:mainapp')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR/'static']
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
