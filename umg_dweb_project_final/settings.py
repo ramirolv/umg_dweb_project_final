d569d4c1d8cc7ba342dd0138d422b8db8f4d7b1e
ramirolv <rlopezv11@miumg.edu.gt>
2022-11-27 23:41:48 -0600

    Update settings.py

--- umg_dweb_project_final/settings.py
+++ umg_dweb_project_final/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+from django.conf import settings
 from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -142,9 +143,12 @@ LOGIN_REDIRECT_URL = reverse_lazy('home:mainapp')
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR/'static']
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
