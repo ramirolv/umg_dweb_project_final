9039b1124d065a7aee449168ae9232f626b5f131
ramirolv <rlopezv11@miumg.edu.gt>
2022-11-27 23:26:03 -0600

    Update settings.py

--- umg_dweb_project_final/settings.py
+++ umg_dweb_project_final/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-^pbx(-_hxzk$frtn_@i936%^c8gac!=2=0xuyk8^$fk47ac(=7'
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-^pbx(-_hxzk$frtn_@i936%^c8gac!=2=0xuyk8^$fk47ac(=7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
