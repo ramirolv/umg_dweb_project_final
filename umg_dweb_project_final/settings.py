a0f71776dea4ef2b17c43bc012ad4dc69aab9f00
ramirolv <rlopezv11@miumg.edu.gt>
2022-11-27 23:03:07 -0600

    Config CRSF Secret key

--- umg_dweb_project_final/settings.py
+++ umg_dweb_project_final/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 from django.urls import reverse_lazy
 
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^pbx(-_hxzk$frtn_@i936%^c8gac!=2=0xuyk8^$fk47ac(=7'
+SECRET_KEY = os.environ['SECRET_KEY']
+#SECRET_KEY = 'django-insecure-^pbx(-_hxzk$frtn_@i936%^c8gac!=2=0xuyk8^$fk47ac(=7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
