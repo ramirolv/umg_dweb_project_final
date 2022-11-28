ebab9f047333adc59730c27c10e34141c8da3de7
ramirolv <rlopezv11@miumg.edu.gt>
2022-11-27 23:46:53 -0600

    collect static

--- staticfiles/js/functions/emergente.js
+++
@@ -1,3 +0,0 @@
-function ventanaSecundaria (URL){ 
-    window.open(URL,"ventana1","width=400,height=500,scrollbars=NO") 
-}
--- umg_dweb_project_final/settings.py
+++ umg_dweb_project_final/settings.py
@@ -146,7 +146,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "staticfiles"),
 )
 
 
