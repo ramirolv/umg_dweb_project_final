7f5b2b6f4a4a35b7d3a4a7d9189cc5b4981a7bf0
ramirolv <rlopezv11@miumg.edu.gt>
2022-11-27 23:45:26 -0600

    Update settings.py

--- umg_dweb_project_final/settings.py
+++ umg_dweb_project_final/settings.py
@@ -142,13 +142,13 @@ LOGIN_REDIRECT_URL = reverse_lazy('home:mainapp')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
